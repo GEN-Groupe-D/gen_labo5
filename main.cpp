4725198904b1fc4603bc3a71a04fe55d36e8fe7b
thomas rieder <thom.rieder@gmail.com>
2020-06-06 12:11:55 +0200

    Travis with gtest test

--- main.cpp
+++ main.cpp
@@ -4,11 +4,21 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 
+using namespace std;
 
-// Simple test, does not use gmock
-TEST(Dummy, foobar)
-{
-    EXPECT_EQ(1, 1);
+//TEST customer statement
+TEST(Customer, Statement){
+
+    string expectedStatement = "Rental Record for Olivier\n\tKarate Kid\t9.5\n\tAvengers: Endgame\t15\n\tSnow White\t1.5\nAmount owed is 26\nYou earned 4 frequent renter points";
+
+    Customer customer("Olivier");
+    customer.addRental( Rental( Movie("Karate Kid"), 7));
+    customer.addRental( Rental( Movie( "Avengers: Endgame", Movie::NEW_RELEASE ), 5));
+    customer.addRental( Rental( Movie("Snow White", Movie::CHILDRENS), 3 ));
+
+    std::cout << customer.statement() << std::endl;
+
+    ASSERT_EQ(customer.statement(), expectedStatement);
 }
 
 
