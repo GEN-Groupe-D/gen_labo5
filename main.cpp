be161cdd2a4b759852cbf8fcc53bacc4e7c1b890
thomas rieder <thom.rieder@gmail.com>
2020-06-06 12:30:39 +0200

    travis test

--- main.cpp
+++ main.cpp
@@ -6,15 +6,30 @@
 
 using namespace std;
 
+struct ConsumerTest : testing::Test {
+
+    Customer customer;
+
+    ConsumerTest() {
+
+        customer = Customer("Olivier");
+        customer.addRental( Rental( Movie("Karate Kid"), 7));
+        customer.addRental( Rental( Movie( "Avengers: Endgame", Movie::NEW_RELEASE ), 5));
+        customer.addRental( Rental( Movie("Snow White", Movie::CHILDRENS), 3 ));
+
+    }
+
+    ~ConsumerTest() {
+
+    }
+
+};
+
+
 //TEST customer statement
-TEST(Customer, Statement){
+TEST_F(ConsumerTest, ConsumerStatementValue){
 
     string expectedStatement = "Rental Record for Olivier\n\tKarate Kid\t9.5\n\tAvengers: Endgame\t15\n\tSnow White\t1.5\nAmount owed is 26\nYou earned 4 frequent renter points";
-
-    Customer customer("Olivier");
-    customer.addRental( Rental( Movie("Karate Kid"), 7));
-    customer.addRental( Rental( Movie( "Avengers: Endgame", Movie::NEW_RELEASE ), 5));
-    customer.addRental( Rental( Movie("Snow White", Movie::CHILDRENS), 3 ));
 
     std::cout << customer.statement() << std::endl;
 
@@ -30,6 +45,7 @@ int main() {
     customer.addRental( Rental( Movie("Snow White", Movie::CHILDRENS), 3 ));
 
     std::cout << customer.statement() << std::endl;
+
 
     ::testing::InitGoogleTest();
     return RUN_ALL_TESTS();
